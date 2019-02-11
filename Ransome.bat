ac13b74c402f91d0b0a4cd4daa085a7b46388e29
MatteeCodes <46092609+MatteeCodes@users.noreply.github.com>
2019-02-11 15:28:05 -0800

    /

---
+++ Ransome.bat
@@ -0,0 +1,7 @@
+@echo off
+FOR /F "tokens=*" %g IN ('dir /b /s') do (SET data=%g)
+echo Your computer has been LOCKED By the RANBASERansomeware! Sorry :(
+cls
+echo Here are files encrypted:
+ping localhost -n 4 >NUL
+echo %data%
